a13960bba4bc1909bc51b9e443ebf27024c068a3
nadyawilliams <nadya@sdsc.edu>
2014-09-09 19:31:32 -0700

    initial

---
+++ src/ejabberd/version.mk
@@ -0,0 +1,8 @@
+PKGROOT		= /opt/ejabberd
+ERLDIR          = /opt/erlang
+NAME    	= ipop-ejabberd
+VERSION 	= 14.07
+ARCHIVENAME        = ejabberd
+JUSER           = ejabberd
+RELEASE 	= 0
+TARBALL_POSTFIX	= tgz
