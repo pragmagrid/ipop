2190fb28a9217b3e82195da94180348adddaaa54
nadyawilliams <nadya@sdsc.edu>
2014-09-16 16:53:35 -0700

    initial for ejabberd

--- src/ejabberd/version.mk
+++ src/ejabberd/version.mk
@@ -1,8 +1,13 @@
-PKGROOT		= /opt/ejabberd
-ERLDIR          = /opt/erlang
+PKGROOT		= /opt/ipop/ejabberd
+ERLDIR          = /opt/ipop/erlang
 NAME    	= ipop-ejabberd
 VERSION 	= 14.07
-ARCHIVENAME        = ejabberd
-JUSER           = ejabberd
+ARCHIVENAME     = ejabberd
+
+EJUSER          = ejabberd
+LOGDIR          = /var/log/ejabberd
+LOCKDIR         = /var/lock/ejabberdctl
+SPOOLDIR        = /var/lib/ejabberd
+
 RELEASE 	= 0
 TARBALL_POSTFIX	= tgz
